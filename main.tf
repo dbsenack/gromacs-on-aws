e7ac295ea9b9cdc519b5b92d9c9c45d936fac56a
David Senack <david.senack@gmail.com>
2024-01-18 22:37:42 -0500

    Add basic VPC and subnet configs

--- main.tf
+++ main.tf
@@ -8,12 +8,32 @@ resource "aws_vpc" "gromacs_vpc" {
 }
 
 # Public Subnet Configuration
-
+resource "aws_subnet" "gromacs_public_subnet" {
+  vpc_id = aws_vpc.gromacs_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+}
 
 # Private Subnet Configuration
+resource "aws_subnet" "gromacs_private_subnet" {
+  vpc_id = aws_vpc.gromacs_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2a"
+}
+
+# TODO add NAT Gateway and Internet Gateway!!
 
 # Head Node Configuration
+resource "aws_instance" "gromacs_head_node" {
+  
+}
 
 # Compute Node(s) Configuration
+resource "aws_autoscaling_group" "gromacs_compute_nodes" {
+  
+}
 
 # File System Configuration
+resource "aws_efs_file_system" "gromacs_file_system" {
+  
+}
