eea3ac9ecb9118bf8b1eeda1c2f974c910bfa4ca
David Senack <david.senack@gmail.com>
2024-01-19 10:29:09 -0500

    Add network resources configuration

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ resource "aws_subnet" "gromacs_public_subnet" {
   vpc_id = aws_vpc.gromacs_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "gromacs"
+  }
 }
 
 # Private Subnet Configuration
@@ -19,9 +23,50 @@ resource "aws_subnet" "gromacs_private_subnet" {
   vpc_id = aws_vpc.gromacs_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "gromacs"
+  }
 }
 
-# TODO add NAT Gateway and Internet Gateway!!
+# Internet and NAT Gateway Configuration
+resource "aws_internet_gateway" "gromacs_internet_gateway" {
+  vpc_id = aws_vpc.gromacs_vpc.id
+
+  tags = {
+    Name = "gromacs"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "gromacs_internet_gateway_attachment" {
+  internet_gateway_id = aws_internet_gateway.gromacs_internet_gateway.id
+  vpc_id = aws_vpc.gromacs_vpc.id
+}
+
+resource "aws_nat_gateway" "gromacs_nat_gateway" {
+  allocation_id = aws_eip.gromacs_eip.id
+  subnet_id = aws_subnet.gromacs_public_subnet.id
+
+  tags = {
+    Name = "gromacs"
+  }
+  
+}
+
+resource "aws_eip" "gromacs_eip" {
+  domain = "vpc"
+
+  instance = aws_instance.gromacs_head_node.id
+  associate_with_private_ip = "10.0.1.12"
+  depends_on = [ aws_internet_gateway_attachment.gromacs_internet_gateway_attachment, aws_internet_gateway.gromacs_internet_gateway ]
+
+  tags = {
+    Name = "gromacs"
+  }
+  
+}
+
+//////////////////////////////
 
 # Head Node Configuration
 resource "aws_instance" "gromacs_head_node" {
