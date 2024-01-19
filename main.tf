c4b661260e3d135e3289145c6d35c059b7a828f5
David Senack <david.senack@gmail.com>
2024-01-19 11:43:26 -0500

    Add node configuration and auto-scaling group

--- main.tf
+++ main.tf
@@ -70,15 +70,113 @@ resource "aws_eip" "gromacs_eip" {
 
 # Head Node Configuration
 resource "aws_instance" "gromacs_head_node" {
-  
+  ami = "ami-0d5d9d301c853a04a"
+  instance_type = "c5a.2xlarge"
+  subnet_id = aws_subnet.gromacs_public_subnet.id
+  associate_public_ip_address = true
+  key_name = "gromacs"
+  vpc_security_group_ids = [ aws_security_group.gromacs_security_group.id ]
+  instance_initiated_shutdown_behavior = terminate
+  tags = {
+    Name = "gromacs"
+  }
 }
 
 # Compute Node(s) Configuration
 resource "aws_autoscaling_group" "gromacs_compute_nodes" {
-  
+  name = "gromacs"
+  max_size = 3
+  min_size = 1
+  desired_capacity = 1
+  launch_configuration = aws_launch_configuration.gromacs_launch_configuration.name
+  vpc_zone_identifier = [ aws_subnet.gromacs_private_subnet.id ]
+  depends_on = [ aws_instance.gromacs_head_node ]
 }
+
+resource "aws_launch_configuration" "gromacs_launch_configuration" {
+  name = "gromacs"
+  image_id = "ami-0d5d9d301c853a04a"
+  instance_type = "c5a.2xlarge"
+  associate_public_ip_address = false
+  key_name = "gromacs"
+  security_groups = [ aws_security_group.gromacs_security_group.id ]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y nfs-utils
+              sudo mkdir /mnt/efs
+              sudo mount -t efs ${aws_efs_file_system.gromacs_file_system.dns_name}:/ /mnt/efs
+              EOF
+}
+
+resource "aws_autoscaling_policy" "gromacs_autoscaling_policy" {
+  name = "gromacs"
+  policy_type = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.gromacs_compute_nodes.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "gromacs_cloudwatch_metric_alarm" {
+  alarm_name = "gromacs"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 2
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = 120
+  statistic = "Average"
+  threshold = 50.0
+  alarm_description = "Scale compute nodes up and down based on CPU utilization"
+  alarm_actions = [ aws_autoscaling_policy.gromacs_autoscaling_policy.arn ]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.gromacs_compute_nodes.name
+  }
+}
+
+//////////////////////////////
+
+
 
 # File System Configuration
 resource "aws_efs_file_system" "gromacs_file_system" {
   
 }
+
+//////////////////////////////
+
+# Security Group Configuration
+resource "aws_security_group" "gromacs_security_group" {
+  name = "gromacs"
+  description = "Security group for GROMACS cluster"
+
+  vpc_id = aws_vpc.gromacs_vpc.id
+
+  ingress {
+    description = "SSH access"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = []
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = []
+  }
+
+  ingress {
+    description = "HTTPS access"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = []
+  }
